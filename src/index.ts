17be7cc108f3e7f1447084c7c3d395ce1df71ba7
Václav Honzík <honzik.vaclav@gmail.com>
2021-07-12 15:55:29 +0200

    Exported IKeyword
    - Added forgotten exports to index.ts for IKeyword and IKeywordGroup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export { Colored, CommandColored } from "./components/Colored";
 export type { ColoredProps } from "./components/Colored";
 export { AutoCompleteResultType, ParsingFailureReason } from './ICommand';
 export type { default as ICommand, AutoCompleteResult, ParsingResult} from './ICommand';
+export type { default as IKeyword, IKeywordGroup} from './IKeyword';
 export * from './Parsers';
 
 import { Gash } from './Gash';
